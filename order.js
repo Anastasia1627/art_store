9bc6a7cfcae8c89a8e8bd9f5d166907631fce8e5
freemanoid <freemanoid321@gmail.com>
2017-08-31 13:06:22 +0300

    Grey out background for shopping bag dialog

--- order.js
+++ order.js
@@ -1,9 +1,12 @@
 ﻿var dialog = document.querySelector('dialog');
+
       document.querySelector('#show').onclick = function() {
+        $('#overlay').fadeIn(0);
         dialog.show();
       };
       document.querySelector('#close').onclick = function() {
         dialog.close();
+        $('#overlay').fadeOut(1000);
       };
 
       function order(){
